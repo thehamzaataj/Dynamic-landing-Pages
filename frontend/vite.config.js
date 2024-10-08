ff3bd453a61ca41a42c6c16d82c092a65b4360f0
thehamzataj <thehamzaataj>
2024-10-08 15:47:37 +0500

    h

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  optimizeDeps: {
+    include: ['react-icons/fa'], 
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-icons/fa'], 
+    },
+  },
+};
